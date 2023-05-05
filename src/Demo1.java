ff1bb521a2b750cca2a510810441f696429bbd92
chinthaka <chinthakaweerasekara9@gmail.com>
2023-05-05 13:48:03 +0530

    4

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,5 +1,12 @@
+import resource.Bus;
+import resource.Car;
+import resource.Van;
+
 public class Demo1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        Van van = new Van();
+        Bus bus = new Bus();
 
     }
 }
